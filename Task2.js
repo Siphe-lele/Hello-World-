35708dbe82e175ee9fc9fb2be0ea89dfcbbf1c28
Siphe-lele <166843219+Siphe-lele@users.noreply.github.com>
2024-12-19 12:50:24 +0200

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,14 @@
+// Printing hello world 
+function task2(para1){
+  if ("Sally"){
+    return "Hello Sally!";
+     }
+     else if ("Tshepo"){
+      return "Hello Tshepo!";
+     }
+    else if (""){
+      return ("Hello Friend!");
+    }
+  }
+  task2();
+  module.export ={task2};
